297b318314b16a625d1fc170b289d8c9325182b4
sundeepnarang <sundeepnrng@gmail.com>
2018-08-17 13:45:03 -0400

    better error handle

--- index.js
+++ index.js
@@ -7,6 +7,8 @@ var _  = require("lodash");
 
 module.exports = function (stream,filePath,options,done) {
     var opts = {};
+    var errored = false;
+    var asyncErr = null;
     if(typeof options === "function"){
         done = options;
     } else{
@@ -17,16 +19,29 @@ module.exports = function (stream,filePath,options,done) {
 
     var wStream = fs.createWriteStream(filePath,opts);
     stream.on('error',function(err){
+        if(!err){
+            err = new Error("Stream Error");
+        }
+        errored = true;
+        asyncErr = err;
         console.log("Error reading stream : ", err);
         return done(err);
     });
 
     wStream.on('error',function(err){
+        if(!err){
+            err = new Error("Stream Error");
+        }
+        errored = true;
+        asyncErr = err;
         console.log("Error writing stream : ", err);
         return done(err);
     });
 
     wStream.on('close',function () {
+        if(errored){
+            return done(asyncErr)
+        }
         done(null,filePath)
     });
 
